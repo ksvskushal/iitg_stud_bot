ba592bc8ff33327b2b13017af33b4d20deab2281
Vivek Raj <raj.vivek.151297@gmail.com>
2018-03-09 17:23:04 +0530

    add db settings

--- __init__.py
+++ __init__.py
@@ -4,11 +4,24 @@ import urllib
 import json
 import os
 
-from flask import Flask
-from flask import request
-from flask import make_response
+
+from flask import Flask,request,make_response
+from flaskext.mysql import MySQL
+import httplib, urllib, json
+import os
+from collections import defaultdict
+
+mysql= MySQL()
 
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'buddy'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
+
 @app.route("/hello")
 def hello():
     return "Hello, I love Digital Ocean!"
@@ -31,3 +44,22 @@ if __name__ == "__main__":
     print "Starting app on port %d" % port
     app.run(debug=True, port=port, host='0.0.0.0')
 
+def get_latest():
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    query_1 = "SELECT * FROM doc_data ORDER BY id DESC LIMIT 0, 1;"
+    cursor.execute(query_1)
+
+    data = cursor.fetchall()
+
+    stri = json.dumps(data)
+
+    return {
+        "speech": stri,
+        "displayText": stri,
+        #"data": {},
+        # "contextOut": [],
+        "source": "chitra"
+    }
