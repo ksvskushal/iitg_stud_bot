1ca34ac92ecc0de6805ba2ac286acc2d0d77de0e
Kushal K S V S <kkushal32@gmail.com>
2018-03-10 04:21:29 +0530

    Syntax Error

--- __init__.py
+++ __init__.py
@@ -84,7 +84,7 @@ def get_class_timings_nfl(req,res):
         query = "SELECT course_id, start_time, room_number FROM ctt WHERE roll_number = " + roll_no + " AND day = \"" + week_day+ "\" ORDER BY start_time LIMIT 1"
     elif nfl == "last" or "Last":
         query = "SELECT course_id, start_time, room_number FROM ctt WHERE roll_number = " + roll_no + " AND day = \"" + week_day+ "\" ORDER BY start_time DESC LIMIT 1"
-    elif nfl = "next" or "Next":
+    elif nfl == "next" or "Next":
         query = "SELECT course_id, start_time, room_number FROM ctt WHERE roll_number = " + roll_no + " AND day = \"" + week_day+ "\" AND start_time > \""+hour+"\"ORDER BY start_time LIMIT 1"
 
     cursor.execute(query)
